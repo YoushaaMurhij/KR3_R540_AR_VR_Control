0b5aa6589f0254f1bc39185941ac538b6bd54e87
Youshaa Murhij <yosha.morheg@gmail.com>
2020-04-12 19:07:02 +0300

    Worked!

---
+++ Assets/Scripts/Axis_6.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Axis_6 : MonoBehaviour
+{
+    private double theta_6;
+    private double d_theta6;
+    internal void Start()
+    {
+        theta_6 = 0;
+    }
+    internal void Update()
+    {
+        theta_6 = MainCode.alpha6;
+        theta_6 = -1 * theta_6;
+        double ACT;
+        if (theta_6 >= -350 && theta_6 <= 350)
+        {
+            if (transform.localEulerAngles.z > 180)
+            {
+                ACT = transform.localEulerAngles.z - 360;
+            }
+            else
+            {
+                ACT = transform.localEulerAngles.z;
+            }
+            d_theta6 = theta_6 - ACT;
+            Quaternion originalRot = transform.rotation;
+            transform.rotation = originalRot * Quaternion.AngleAxis((float)d_theta6, Vector3.forward);
+        }
+    }
+}
--- Assets/Scripts/MainCode.cs
+++ Assets/Scripts/MainCode.cs
@@ -5,9 +5,9 @@ using System;
 using System.Linq;
 using System.Text;
 using System.Threading;
-//using Leap;
-//using Leap.Unity;
-//using Leap.Unity.Attributes;
+using Leap;
+using Leap.Unity;
+using Leap.Unity.Attributes;
 public class Variables
 {
     public static double[] xyz_ref;
@@ -16,26 +16,21 @@ public class Variables
 }
 public class MainCode : MonoBehaviour
 {
-    //public LeapServiceProvider provider;
+    public LeapServiceProvider provider;
     double x, y, z, X = 0, Y = 0, Z = 0;
     double[] joints;
     double[] home_joints = { 0, -45, 45, 1, 45, 5 }; // home joints, in deg
     public static int alpha1 = 0, alpha2 = -90, alpha3 = 90, alpha4 = 0, alpha5 = 0, alpha6 = 0;
-    //public bool LeapBOOL = true;
+    public bool LeapBOOL = true;
     public bool BO2 = true;
+    int Factor_VR = 0;
+    int Factor_LM = 80; //400
     void Start()
     {
         //================================RoboDK Code==============================================
         RoboDK RDK = new RoboDK();
         Variables.ROBOT = RDK.ItemUserPick("Select a robot", RoboDK.ITEM_TYPE_ROBOT);
-        if (Variables.ROBOT.Connect())
-        {
-            RDK.setRunMode(RoboDK.RUNMODE_RUN_ROBOT);
-        }
-        else
-        {
-            RDK.setRunMode(RoboDK.RUNMODE_SIMULATE);
-        }
+        RDK.setRunMode(RoboDK.RUNMODE_SIMULATE);
         //Variables.ROBOT.MoveJ(home_joints);
         Mat frame = Variables.ROBOT.PoseFrame();
         //Mat tool = Variables.ROBOT.PoseTool();
@@ -47,44 +42,28 @@ public class MainCode : MonoBehaviour
         //Variables.ROBOT.setPoseTool(tool);    // set the tool frame: important for Online Programming
         Variables.ROBOT.setSpeed(500);        // Set Speed to 100 mm/s
         Variables.ROBOT.setZoneData(5);       // set the rounding instruction 
-        //Variables.ROBOT.MoveL(pose_ref);
     }
     void Update()
     {
-        int Factor_VR = 0;
-        //int Factor_LM = 400; //400
-        //Frame frame = provider.CurrentFrame;
-        //if (frame != null)
-        //{
-        //    if (frame.Hands.Count > 0)
-        //    //foreach (Hand hand in frame.Hands)
-        //    {
-        //        Hand hand = frame.Hands[0];
-        //        X = hand.PalmPosition.z;
-        //        Y = hand.PalmPosition.x;
-        //        Z = hand.PalmPosition.y;
-        //    }
-        //Vector3 handPosition = hand.PalmPosition.ToVector3();
-        //x = Variables.xyz_ref[0] + X * Factor_LM;
-        //y = Variables.xyz_ref[1] - Y * Factor_LM;
-        //z = Variables.xyz_ref[2] + Z * Factor_LM;
-
-        //using (System.IO.StreamWriter file =
-        //new System.IO.StreamWriter(@"C:\Users\Youshaa Murhij\Desktop\pos11.txt", true))
-        //{
-        //    file.WriteLine(x + " " + y + " " + z);
-        //}
-        //using (System.IO.StreamWriter file =
-        //new System.IO.StreamWriter(@"C:\Users\Youshaa Murhij\Desktop\pos12.txt", true))
-        //{
-        //    file.WriteLine((Variables.xyz_ref[0] + X * 65) + " " + (Variables.xyz_ref[1] - Y * 65) + " " + (Variables.xyz_ref[2] + Z * 65));
-        //}
-        //}
-        x = Variables.xyz_ref[0] + CPos.v.z * Factor_VR;
-        y = Variables.xyz_ref[1] - CPos.v.x * Factor_VR;
-        z = Variables.xyz_ref[2] + CPos.v.y * Factor_VR;
-        //Variables.target_pose.setPos(x, y, z);
-        //Variables.ROBOT.MoveL(Variables.target_pose);
+        Frame frame = provider.CurrentFrame;
+        if (frame != null)
+        {
+           if (frame.Hands.Count > 0)
+           {
+               Hand hand = frame.Hands[0];
+               X = hand.PalmPosition.z;
+               Y = hand.PalmPosition.x;
+               Z = hand.PalmPosition.y;
+           }
+        x = Variables.xyz_ref[0] + X * Factor_LM;
+        y = Variables.xyz_ref[1] - Y * Factor_LM;
+        z = Variables.xyz_ref[2] + Z * Factor_LM;
+        // x = Variables.xyz_ref[0] + CPos.v.z * Factor_VR;
+        // y = Variables.xyz_ref[1] - CPos.v.x * Factor_VR;
+        // z = Variables.xyz_ref[2] + CPos.v.y * Factor_VR;
+        }
+        Variables.target_pose.setPos(x, y, z);
+        Variables.ROBOT.MoveL(Variables.target_pose);
         joints = Variables.ROBOT.Joints();
         //print(joints[0]);
         alpha1 = (int)joints[0];
@@ -93,11 +72,8 @@ public class MainCode : MonoBehaviour
         alpha4 = (int)joints[3];
         alpha5 = (int)joints[4];
         alpha6 = (int)joints[5];
+        
     }
-
-
-
-
 
     //void OnGUI()
     //{
